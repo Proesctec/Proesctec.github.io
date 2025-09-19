06cb349ac2918c0310ad5e6a66806d4439449769
YaelZamora <ajedrezkasparov@gmail.com>
2025-09-19 13:34:21 -0600

    Arreglo de error al entrar a Drive

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2023790906"
+    serviceWorkerVersion: "3267467884"
   }
 });
